f12f09735193ce1df19a24065154ff82ee939833
Karrix <tim.dautreme@hotmail.fr>
2019-08-24 21:56:32 +0200

    Update

--- ProtoDeep/srcs/prep/prep_stdiz.c
+++ ProtoDeep/srcs/prep/prep_stdiz.c
@@ -23,7 +23,6 @@ t_farr   prep_minmax_scal_stdiz_init(t_farr x, t_prep_data *pdata)
     som = 0;
     pdata->min = math_min_a(x);
     pdata->maxsmin = math_max_a(x) - pdata->min;
-    //y = malloc(sizeof(float) * len);
     y = dast_new_farr(x.len);
     for (size_t i = 0; i < y.len; i++)
     {
