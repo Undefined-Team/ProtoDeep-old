ecde54ef801f7569d5d9c7bb9a4ae7cca96e7c5b
Timothée DAUTREME <tim.dautreme@hotmail.fr>
2019-09-01 23:03:56 +0200

    nn update

--- ProtoDeep/includes/pd_nn/nn_master.h
+++ ProtoDeep/includes/pd_nn/nn_master.h
@@ -6,27 +6,31 @@
 // Structures
 typedef enum {PD_L_DENSE, PD_L_CONVOLUTION, PD_L_MAXPOOL} pd_layer_type;
 
-typedef struct		pds_tensor
-{
-	void			*values;
-	size_t			rank;
-	size_t			len;
-}					pd_tensor;
+typedef struct				pds_tensor {
+	void					*values;
+	size_t					rank;
+	size_t					len;
+}							pd_tensor;
 
-typedef struct		pds_layer
-{
-	pd_layer_type	layer_type;
-	pd_tensor		tensor;
-}					pd_layer;
+typedef struct				pds_layer {
+	pd_layer_type			layer_type;
+	pd_tensor				tensor;
+	void					*param;
+}							pd_layer;
 
-typedef struct		pds_network
-{
-	pd_tensor		*layers;
-	size_t			len;
-}					pd_network;
+typedef struct				pds_layer_lst {
+	pd_tensor				layer;
+	struct pds_layer_list 	*next;
+}							pd_layer_lst;
+
+typedef struct				pds_network {
+	pd_layer_lst			*init_layers;
+	pd_tensor				*layers;
+	size_t					len;
+}							pd_network;
 
 // Prototypes
-void				pd_nn_master(void *input);
-pd_tensor			pd_get_output_dense(pd_tensor output, pd_tensor layer);
+void						pd_nn_master(void *input);
+pd_tensor					pd_get_output_dense(pd_tensor output, pd_tensor layer);
 
 #endif
