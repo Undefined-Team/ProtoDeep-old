b95ab2b5b7cce2ec52aad1dc1230a0877c006d81
Timothée DAUTREME <tim.dautreme@hotmail.fr>
2019-09-01 19:02:48 +0200

    Change norm

--- ProtoDeep/includes/pd_csv/pd_prep.h
+++ ProtoDeep/includes/pd_csv/pd_prep.h
@@ -42,27 +42,27 @@ typedef struct              pds_name_index {
 }                           pd_name_index;
 
 // Prototypes
-pd_float_a                   prep_minmax_scal(pd_float_a x);
-pd_float_a                   prep_stdiz(pd_float_a x, pd_stdiz_data pdata);
-pd_float_a                   prep_stdiz_init(pd_float_a x, pd_stdiz_data *pdata);
-pd_stdiz_a                   prep_strandardize_init(pd_csv csv);
-void                        prep_standardize(pd_csv csv, pd_stdiz_a std_data);
+pd_float_a                   pd_prep_minmax_scal(pd_float_a x);
+pd_float_a                   pd_prep_stdiz(pd_float_a x, pd_stdiz_data pdata);
+pd_float_a                   pd_prep_stdiz_init(pd_float_a x, pd_stdiz_data *pdata);
+pd_stdiz_a                   pd_prep_strandardize_init(pd_csv csv);
+void                        pd_prep_standardize(pd_csv csv, pd_stdiz_a std_data);
 
-void                        prep_free_ohe_tree(pd_ohe_trees *ohe_trees);
-void                        prep_add_line(pd_csv_col *begin_col, size_t col_index, size_t line_index);
-pd_csv_col                   *prep_add_col(pd_csv_col *laspd_col, size_t nbr_line, size_t max_line, pd_char_a name);
-pd_ohe_trees                 *prep_ohe_init(pd_csv *csv, pd_str_a col_names);
-void                        prep_ohe(pd_csv *csv, pd_ohe_trees *tbegin);
-void                        prep_delete(pd_csv *csv, pd_str_a col_names);
+void                        pd_prep_free_ohe_tree(pd_ohe_trees *ohe_trees);
+void                        pd_prep_add_line(pd_csv_col *begin_col, size_t col_index, size_t line_index);
+pd_csv_col                   *pd_prep_add_col(pd_csv_col *laspd_col, size_t nbr_line, size_t max_line, pd_char_a name);
+pd_ohe_trees                 *pd_prep_ohe_init(pd_csv *csv, pd_str_a col_names);
+void                        pd_prep_ohe(pd_csv *csv, pd_ohe_trees *tbegin);
+void                        pd_prep_delete(pd_csv *csv, pd_str_a col_names);
 
-pd_csv_conf                  prep_inipd_conf(pd_str_a delete_indexs, pd_str_a ohe_indexs);
-void                        prep_free_conf(pd_csv_conf conf);
-void                        prep_prepare(pd_csv *csv, pd_csv_conf *conf);
-void                        prep_all_to_float(pd_csv csv);
-int                         prep_csv_split(pd_csv csv, pd_csv *train, pd_csv *test, float f_split);
+pd_csv_conf                  pd_prep_inipd_conf(pd_str_a delete_indexs, pd_str_a ohe_indexs);
+void                        pd_prep_free_conf(pd_csv_conf conf);
+void                        pd_prep_prepare(pd_csv *csv, pd_csv_conf *conf);
+void                        pd_prep_all_to_float(pd_csv csv);
+int                         pd_prep_csv_split(pd_csv csv, pd_csv *train, pd_csv *test, float f_split);
 
-pd_tbnode                    *prep_new_tbnode(char c, int word_index);
-void                        prep_free_tbnode(pd_tbnode *begin);
-pd_str_a                     prep_name_sort(pd_csv csv, pd_str_a col_ni);
+pd_tbnode                    *pd_prep_new_tbnode(char c, int word_index);
+void                        pd_prep_free_tbnode(pd_tbnode *begin);
+pd_str_a                     pd_prep_name_sort(pd_csv csv, pd_str_a col_ni);
 
 #endif
--- ProtoDeep/includes/pd_dast.h
+++ ProtoDeep/includes/pd_dast.h
@@ -11,20 +11,20 @@
 # define pd_arrFree(w)             pd_dast_free_arr(w, 0)
 # define pd_arrRFree(w, v)         pd_dast_free_arr(w, v)
 
-# define pd_strNew(z)              pd_arrNew(T_CHAR, 1, z)
-# define pd_strSNew(z)             pd_arrSNew(T_CHAR, 1, z)
+# define pd_strNew(z)              pd_arrNew(PD_T_CHAR, 1, z)
+# define pd_strSNew(z)             pd_arrSNew(PD_T_CHAR, 1, z)
 # define pd_strFree(w)             pd_arrRFree(w, 1)
 
-# define pd_t_char_a               pd_pd_arr
-# define pd_t_float_a              pd_t_arr
-# define pd_t_size_t_a             pd_t_arr
-# define pd_t_str_a                pd_t_arr
-# define pd_t_stdiz_a              pd_t_arr
+# define pd_char_a               pd_arr
+# define pd_float_a              pd_arr
+# define pd_size_t_a             pd_arr
+# define pd_str_a                pd_arr
+# define pd_stdiz_a              pd_arr
 
 # define pd_PROT_MALLOC(x)         if (!(x)) {return NULL;}
-# define pd_PROT_ARR_TYPE(x, y)    if (x != y) {return arrNew(y, 0, NULL);}
+# define pd_PROT_ARR_TYPE(x, y)    if (x != y) {return pd_arrNew(y, 0, NULL);}
 
-# define pd_T_STR                  T_ARR // T_STR is an array of array
+# define PD_T_STR                  T_ARR // T_STR is an array of array
 
 // Structures
 typedef enum {false,true} bool;
@@ -37,10 +37,10 @@ typedef struct                  pds_arr {
 }                               pd_arr;
 
 // Prototypes
-pd_arr                           dast_init_arr(pd_type type, size_t len);
-pd_arr                           dast_new_s_arr(pd_type type, size_t len, void* val);
-pd_arr                           dast_new_arr(pd_type type, size_t len, void* val);
-void                            dast_free_arr(pd_arr arr, int depth);
+pd_arr                           pd_dast_init_arr(pd_type type, size_t len);
+pd_arr                           pd_dast_new_s_arr(pd_type type, size_t len, void* val);
+pd_arr                           pd_dast_new_arr(pd_type type, size_t len, void* val);
+void                            pd_dast_free_arr(pd_arr arr, int depth);
 
 void	        pd_dast_free(void **ap);
 
