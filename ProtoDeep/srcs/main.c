a59f9288951d31f290a02f9c8530f0c88a4ade98
sbedene <sylvainbedene@gmail.com>
2019-09-01 19:03:06 +0200

    more pd_

--- ProtoDeep/srcs/main.c
+++ ProtoDeep/srcs/main.c
@@ -10,19 +10,19 @@ pd_csv   pd_get_test_csv_1(void)
 
 
     pd_char_a col3[4] = {pd_strSNew("Gros"), pd_strSNew("Pitit"), pd_strSNew("Moyen"), pd_strSNew("Moyen")};
-    tmp = pd_csv_new_col(pd_arrSNew(T_STR, 4, col3), pd_strSNew("Zizi"));
+    tmp = pd_csv_new_col(pd_arrSNew(PD_T_STR, 4, col3), pd_strSNew("Zizi"));
     csv_test.begin = tmp;
 
     float col1[4] = {200.77777, 1, -1, -200};
-    tmp->next = pd_csv_new_col(pd_arrSNew(T_FLOAT, 4, col1), pd_strSNew("Qi"));
+    tmp->next = pd_csv_new_col(pd_arrSNew(PD_T_FLOAT, 4, col1), pd_strSNew("Qi"));
     tmp = tmp->next;
 
     pd_char_a col2[4] = {pd_strSNew("Timothée"), pd_strSNew("Sylvain"), pd_strSNew("Paco"), pd_strSNew("Tony")};
-    tmp->next = pd_csv_new_col(pd_arrSNew(T_STR, 4, col2), pd_strSNew("Name"));
+    tmp->next = pd_csv_new_col(pd_arrSNew(PD_T_STR, 4, col2), pd_strSNew("Name"));
     tmp = tmp->next;
 
     pd_char_a col4[4] = {pd_strSNew("Ca va"), pd_strSNew("Musclée"), pd_strSNew("Musclée"), pd_strSNew("Musclée")};
-    tmp->next = pd_csv_new_col(pd_arrSNew(T_STR, 4, col4), pd_strSNew("Main droite"));
+    tmp->next = pd_csv_new_col(pd_arrSNew(PD_T_STR, 4, col4), pd_strSNew("Main droite"));
     tmp = tmp->next;
 
     return csv_test;
@@ -38,19 +38,19 @@ pd_csv   pd_get_test_csv_2(void)
 
 
     pd_char_a col3[4] = {pd_strSNew("Gros"), pd_strSNew("Pitit"), pd_strSNew("JSP"), pd_strSNew("Moyen")};
-    tmp = pd_csv_new_col(pd_arrSNew(T_STR, 4, col3), pd_strSNew("Zizi"));
+    tmp = pd_csv_new_col(pd_arrSNew(PD_T_STR, 4, col3), pd_strSNew("Zizi"));
     csv_test.begin = tmp;
 
     float col1[4] = {200.77777, 1, -1, -200};
-    tmp->next = pd_csv_new_col(pd_arrSNew(T_FLOAT, 4, col1), pd_strSNew("Qi"));
+    tmp->next = pd_csv_new_col(pd_arrSNew(PD_T_FLOAT, 4, col1), pd_strSNew("Qi"));
     tmp = tmp->next;
 
     pd_char_a col2[4] = {pd_strSNew("Timothée"), pd_strSNew("Sylvain"), pd_strSNew("Paco"), pd_strSNew("Tony")};
-    tmp->next = pd_csv_new_col(pd_arrSNew(T_STR, 4, col2), pd_strSNew("Name"));
+    tmp->next = pd_csv_new_col(pd_arrSNew(PD_T_STR, 4, col2), pd_strSNew("Name"));
     tmp = tmp->next;
 
     pd_char_a col4[4] = {pd_strSNew("Ca va"), pd_strSNew("Musclée"), pd_strSNew("Musclée"), pd_strSNew("JSP")};
-    tmp->next = pd_csv_new_col(pd_arrSNew(T_STR, 4, col4), pd_strSNew("Main droite"));
+    tmp->next = pd_csv_new_col(pd_arrSNew(PD_T_STR, 4, col4), pd_strSNew("Main droite"));
     tmp = tmp->next;
 
     return csv_test;
@@ -64,7 +64,7 @@ void    csv_with_conf()
 
     pd_char_a del_1[1] = {pd_pd_strSNew("Name")};
     pd_char_a ohe_1[2] = {pd_pd_strSNew("Main droite"), pd_strSNew("Zizi")};
-    pd_csv_conf conf = pd_prep_init_conf(pd_arrSNew(T_STR, 1, del_1), pd_arrSNew(T_STR, 2, ohe_1));
+    pd_csv_conf conf = pd_prep_init_conf(pd_arrSNew(PD_T_STR, 1, del_1), pd_arrSNew(PD_T_STR, 2, ohe_1));
     pd_prep_prepare(&csv_1, &conf);
     pd_csv_print(csv_1);
 
@@ -90,11 +90,11 @@ void    pd_csv_manual()
     pd_csv_print(csv_1);
 
     pd_char_a del_1[1] = {pd_strSNew("Name")};
-    pd_str_a  del_str = pd_arrSNew(T_STR, 1, del_1);
+    pd_str_a  del_str = pd_arrSNew(PD_T_STR, 1, del_1);
     prep_delete(&csv_1, del_str);
 
     pd_char_a ohe_1[2] = {pd_strSNew("Main droite"), pd_strSNew("Zizi")};
-    pd_str_a  ohe_str = pd_arrSNew(T_STR, 2, ohe_1);
+    pd_str_a  ohe_str = pd_arrSNew(PD_T_STR, 2, ohe_1);
     pd_ohe_trees *ohe_trees = pd_prep_ohe_init(&csv_1, ohe_str);
 
     pd_prep_all_to_float(csv_1);
