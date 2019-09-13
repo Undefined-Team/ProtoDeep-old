b4d2e77181de580282d882ace4c96cc91242fcd2
sbedene <sylvainbedene@gmail.com>
2019-09-13 17:39:28 +0200

    ntens

---
+++ ProtoDeep/includes/pd_ntens.h
@@ -0,0 +1,22 @@
+#ifndef PD_NTENS_H
+# define PD_NTENS_H
+
+// Lib
+
+// Macro
+
+// Structures
+typedef struct  pds_ntensor {
+    float       *val;
+    size_t      len;
+    size_t      *shape;
+    size_t      *shape_m;
+    size_t      *shape_len;
+}               pd_ntensor;
+
+// Prototypes
+pd_ntensor *pd_ntens_init_val(pd_arr *shape, float new_val)
+float       pd_ntens_get_val(pd_ntens tensor, ...);
+float       *pd_ntens_get_pval(pd_ntens tensor, ...);
+
+#endif
