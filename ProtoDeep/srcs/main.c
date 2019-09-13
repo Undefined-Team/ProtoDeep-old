e908b76bcdfb6aac4797f69839ff1dfad2cffeeb
sbedene <sylvainbedene@gmail.com>
2019-09-13 23:45:54 +0200

    conflict in matrixdot

--- ProtoDeep/srcs/main.c
+++ ProtoDeep/srcs/main.c
@@ -288,7 +288,7 @@ void    new_transpose_main(void)
     // }
 
     
-    pd_tensor *test = pd_tens_init_mval(pd_arr_shape(1, 1000000000), 2);
+    pd_tensor *test = pd_tens_init_val(pd_arr_shape(1, 1000000000), 2);
     float val;
     for (size_t i = 0; i < 10; i++)
         val = pd_tens_get_mval(test, 999999999);
